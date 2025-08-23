f0c340d96d1ad96c6b64f3d2fa3335c54b145a24
chong <chong@itz.com>
2025-08-23 21:33:07 +0800

    fix: init.sh Makefile

---
+++ Makefile
@@ -0,0 +1,10 @@
+update:
+	bash init.sh
+x86:
+	cd cpp-ipc && make $@
+arm:
+	cd cpp-ipc && make $@
+install:
+	cd cpp-ipc && make $@
+clean:
+	cd cpp-ipc && make $@
--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+git submodule update --init
+
 if [ ! -d cpp-ipc/es_tools ]; then
-	cp -rf es_tools cpp-ipc/
-	cp cpp-ipc/es_tools/makefile cpp-ipc/
-	cp CMakeLists.txt cpp-ipc
-	bash cpp-ipc/es_tools/shell/es_replace cpp-ipc libipc
+	cd cpp-ipc
+	cp ../es_tools ./ -rf
+	cp ../CMakeLists.txt ./
+	cp es_tools/makefile ./
+	bash es_tools/shell/es_replace cpp-ipc libipc
 fi
 
